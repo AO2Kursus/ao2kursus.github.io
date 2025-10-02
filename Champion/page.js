ddff1c12d3a40cd70dbd6f523d61ab1ba7ee0540
christoffera <chris.andreassen@gmail.com>
2025-10-02 16:37:10 +0200

    Add files via upload

--- Champion/page.js
+++ Champion/page.js
@@ -1,8 +1,8 @@
-var polyWidth = 30;	//px bredde på hexagonerne
+var polyWidth = 15;	//px bredde på hexagonerne
 var polyMargin = 3;	//px luft mellem hexagonerne
 
 var coll = Math.ceil($(window).width() / (polyWidth + polyMargin))
-var row = Math.ceil($(window).height() / ((polyWidth * 1.1574)+polyMargin))
+var row = Math.ceil($(window).height() / ((polyWidth)))
 
 var idNum = 0;
 
@@ -28,48 +28,23 @@ for (var ii = 1; ii <= row; ii++){
 		}
 		var svgElement = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
 		svgElement.setAttribute("points",
-											(xMid + xOffset) +','+ yOffset +' '+
-											(polyWidth + xOffset) +','+ (yP1 + yOffset) +' '+
-											(polyWidth + xOffset) +','+ (yP2 + yOffset) +' '+
-											(xMid + xOffset) +','+ (yP3 + yOffset) +' '+
-											xOffset +','+ (yP2 + yOffset) +' '+
-											xOffset +','+ (yP1 + yOffset));
+			(xMid + xOffset) +','+ yOffset +' '+
+			(polyWidth + xOffset) +','+ (yP1 + yOffset) +' '+
+			(polyWidth + xOffset) +','+ (yP2 + yOffset) +' '+
+			(xMid + xOffset) +','+ (yP3 + yOffset) +' '+
+			xOffset +','+ (yP2 + yOffset) +' '+
+			xOffset +','+ (yP1 + yOffset));
 		svgElement.setAttribute("id","poly" + idNum);
 		svgElement.style.display = "none";
 		svgLayer.appendChild(svgElement);
 	}
 }
 
-/*
-var svgLayer = document.getElementById("svgLayer");
-var svgElement = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-svgElement.setAttribute("width",200); 
-svgElement.setAttribute("height",100); 
-svgElement.setAttribute("x",10); 
-svgElement.setAttribute("y",10); 
-svgElement.setAttribute("rx",20); 
-svgElement.setAttribute("ry",20); 
-svgElement.setAttribute("fill","blue");
-svgElement.style.display = "auto";
-svgLayer.appendChild(svgElement);
-*/
-
-
-
-
-
-//dreamFade(Math.floor(Math.random()*22)+1);
-for(var ii = 1; ii <= 25; ii++){
-	dreamFade2();
+for(var ii = 1; ii <= idNum/60; ii++){
+	dreamFade();
 }
-setInterval(dreamFade2,200);
-/*
-function dreamFade(poly){
-	$("#poly" + poly).fadeIn(Math.random()*5*1000, function(){
-		$("#poly" + poly).fadeOut(Math.random()*5*1000)
-});
-}*/
-function dreamFade2(){
+setInterval(dreamFade,idNum/200);
+function dreamFade(){
 	var poly = Math.floor(Math.random()*idNum)+1;
 	$("#poly" + poly).fadeIn(Math.random()*5*1000, function(){
 		$("#poly" + poly).fadeOut(Math.random()*5*1000)
